81cb290e8f533234e741e41bb9d479a67ddf2430
Vishwesh Bankwar <vishweshbankwar@users.noreply.github.com>
2021-10-11 17:33:35 -0700

    Add files via upload

---
+++ ActivityExportService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestDotnet6ActivitySource
+{
+    public class ActivityExportService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var listener = new ActivityListener
+                {
+                    ShouldListenTo = _ => true,
+                    Sample = (ref ActivityCreationOptions<ActivityContext> options) => ComputeActivitySamplingResult(options),
+                    ActivityStarted = (activity) =>
+                    {
+                        Console.WriteLine("Activity Started:");
+                        Console.WriteLine("ParentId: " + activity.ParentId);
+                        Console.WriteLine("ParentSpanId:" + activity.ParentSpanId);
+                        Console.WriteLine("Activity TraceId: " + activity.TraceId);
+                    },
+
+                    // Callback when Activity is stopped.
+                    ActivityStopped = (activity) =>
+                    {
+                        Console.WriteLine("Activity Stopped");
+                    },
+                };
+                ActivitySource.AddActivityListener(listener);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private static ActivitySamplingResult ComputeActivitySamplingResult(
+        in ActivityCreationOptions<ActivityContext> options)
+        {
+            var traceID = options.TraceId; // traceId here will be the traceId for created Activity.
+            var parent = options.Parent; // Parent not available here.
+
+            return ActivitySamplingResult.AllDataAndRecorded;
+        }
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestDotnet6ActivitySource.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            //ActivitySource source = new ActivitySource("Test");
+            //var activity = source.StartActivity("TestActivity");
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
